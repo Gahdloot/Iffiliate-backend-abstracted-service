fa8c99d1e516cd72428609ddd52382a8c9a89ea8
Michael David <davidlearn27@gmail.com>
2023-02-13 21:56:14 +0100

    added All Logistics class with Just A list of companies

--- iffiliate/logistics/views.py
+++ iffiliate/logistics/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import LogisticCompany, LogisticPrice, Dispatch
+from .serializers import LogisticCompanySerializer, LogisticPriceSerializer
 # Create your views here.
+
+
+class AllLogisticsInfo:
+
+    def Companies(self):
+        companies = LogisticCompany.objects.all()
+        companies = LogisticCompanySerializer(data=companies, many=True)
+        serialized_data = companies.data
+        return serialized_data
+
+
