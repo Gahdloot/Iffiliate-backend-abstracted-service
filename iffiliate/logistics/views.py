c30baa76fc07b58e4ff41d8002e116eef33cc5a6
Michael David <davidlearn27@gmail.com>
2023-02-13 22:54:44 +0100

    added class for viewing single logistics company and price

--- iffiliate/logistics/views.py
+++ iffiliate/logistics/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .models import LogisticCompany, LogisticPrice, Dispatch
-from .serializers import LogisticCompanySerializer, LogisticPriceSerializer
+from .models import LogisticCompany, LogisticPrice, Dispatch, Rider
+from .serializers import LogisticCompanySerializer, LogisticPriceSerializer, RidersSerializer, DispatchSerializer
 # Create your views here.
 
 
-class AllLogisticsInfo:
+class AllInfo:
 
     def Companies(self):
         companies = LogisticCompany.objects.all()
@@ -12,4 +12,42 @@ class AllLogisticsInfo:
         serialized_data = companies.data
         return serialized_data
 
+    def Companies_price(self):
+        companies_price = LogisticPrice.objects.all()
+        companies_price = LogisticPriceSerializer(data=companies_price, many=True)
+        serialized_data = companies_price.data
+        return serialized_data
 
+    def Rider(self):
+        riders = Rider.objects.all()
+        riders = RidersSerializer(data=riders, many=True)
+        serialized_data = riders.data
+        return serialized_data
+
+    def Dispatch(self):
+        dispatch = Dispatch.objects.all()
+        dispatch = DispatchSerializer(data=dispatch, many=True)
+        serialized_data = dispatch.data
+        return serialized_data
+
+
+class SingleInfo:
+    def __init__(self, company_name):
+        self.name = company_name
+
+        if company_name is None:
+            raise NameError('company\'s name cannot be None ')
+        try:
+            company = LogisticCompany.objects.get(name=company_name)
+        except LogisticCompany.DoesNotExist:
+            raise LookupError('company\'s name doesnot exist')
+        self.company = company
+
+    def Company(self):
+        company = LogisticCompanySerializer(data=self.company)
+        serialized_data = company.data
+    def Companies_price(self):
+        companies_price = LogisticPrice.objects.get(company=self.company)
+        companies_price = LogisticPriceSerializer(data=companies_price)
+        serialized_data = companies_price.data
+        return serialized_data
